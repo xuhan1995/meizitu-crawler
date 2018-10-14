a7d3454192c5002bbd9df44171445ed531a9742b
hanxu <2017111600@bupt.edu.cn>
2018-10-15 01:16:57 +0800

    fix: add errorhandler and complete rmCachePath

--- utils.js
+++ utils.js
@@ -40,11 +40,8 @@ module.exports = {
       files.forEach((filename) => {
         const file = path.join(albumPath, filename)
         count++
-        console.log(file)
-        console.log(count)
       })
       const repeat = count
-      console.log('======================================', repeat)
       return [true, cachePath, repeat]
     }else{
       fs.mkdirSync(cachePath)
@@ -72,51 +69,56 @@ module.exports = {
   getImgUrl: res => {
     try {
       const $ = cheerio.load(res)
-      return $('.main-image img')[0].attribs.src
+      const img = $('.main-image img')[0]
+      if (img) {
+        return img.attribs.src
+      }
     } catch (error) {
       throw error
     }
   },
 
-  rmCachePath: (cachePath, repeat) => {
+  rmCachePath: (cachePath, repeat, imgNums) => {
     const albumPath = path.resolve(cachePath)
-    if (repeat) {
+    if (repeat) {  //不为0说明没下载完，需要删除所有图片
       files = fs.readdirSync(albumPath)
       files.forEach((filename) => {
           const filePath = path.join(albumPath, filename)
           fs.unlinkSync(`${filePath}`)
+          console.log(
+            chalk.blue(`${cachePath} => 有${repeat}张，应有${imgNums}张,删除成功`)
+          )
       })
-      fs.rmdirSync(albumPath)
     }
-    fs.rmdirSync(albumPath)
   },
 
   downloadImg: async(imgPageUrl, imgUrl, cachePath, index) => {
-    if (imgUrl) {
-      let headers = {
-        Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
-        "Cache-Control": "no-cache",
-        Host: "i.meizitu.net",
-        Pragma: "no-cache",
-        "Proxy-Connection": "keep-alive",
-        Referer: imgPageUrl,
-        "Upgrade-Insecure-Requests": 1,
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.19 Safari/537.36"
-      };//反防盗链
-      await rp({  //await保证爬完一张才打log下载成功
-        url: imgUrl,
-        resolveWithFullResponse: true,
-        headers
-      }).pipe(fs.createWriteStream(`${cachePath}/${index}.jpg`))
-      console.log(
-        chalk.green(`${cachePath}/${index}.jpg下载成功`)
-      )
-    } else {
-      console.log(
-        chalk.red(`${imgUrl}图片不存在`)
-      )
+      try {
+        let headers = {
+          Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+          "Accept-Encoding": "gzip, deflate",
+          "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+          "Cache-Control": "no-cache",
+          Host: "i.meizitu.net",
+          Pragma: "no-cache",
+          "Proxy-Connection": "keep-alive",
+          Referer: imgPageUrl,
+          "Upgrade-Insecure-Requests": 1,
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.19 Safari/537.36"
+        };//反防盗链
+        await rp({  //await保证爬完一张才打log下载成功
+          url: imgUrl,
+          resolveWithFullResponse: true,
+          headers
+        }).pipe(fs.createWriteStream(`${cachePath}/${index}.jpg`))
+        console.log(
+          chalk.green(`${cachePath}/${index}.jpg下载成功`)
+        )
+      } catch (error) {
+        console.log(
+          chalk.red(`${imgUrl}图片不存在`)
+        )
+        throw error
+      }
     }
   }
-}
