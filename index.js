09f53eecfc63e117a8d10879f61c87b6bc4b7610
hanxu <2017111600@bupt.edu.cn>
2018-10-15 14:17:39 +0800

    feature: add stdin and stdout to get page number

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const async = require('async')
 const chalk = require('chalk')
 
 const baseUrl = 'http://www.mzitu.com/page/'
-let nowPageNum = 1 
-const endPageNum = 6
-let complicatingNum = 0
+let nowPageNum, endPageNum, complicatingNum = 0
 
 const main = async url => {
   let albumsInfo = []
@@ -44,11 +42,47 @@ const handleAlum = async (album, callback) => {
     }
   }else {
     console.log(
-      chalk.magenta(`${cachePath} => 已经全部下载成功`)
+      chalk.magenta(`${cachePath} => 已经全部下载成功不需要下载`)
     )
   }
   complicatingNum--
   callback(null)
 }
 
-main(baseUrl + nowPageNum)
+
+process.stdin.setEncoding('utf8')
+process.stdout.write('请输入开始爬取的页码:')
+
+process.stdin.on('data', (chunk) => {
+  chunk = chunk.slice(0,-2)
+  let assgin = Number(chunk)
+
+  if (Number.isNaN(assgin) || assgin < 1 || assgin > 196) {
+    if (!nowPageNum) {
+      process.stdout.write('请输入正确的开始爬取页码:')
+      return
+    }
+    process.stdout.write('请输入正确的结束爬取页码:')
+    return
+  }
+
+  if (!nowPageNum) {  //经过上面验证输入，开始页码已经能符合要求
+    nowPageNum = assgin
+    process.stdout.write('请输入结束爬取的页码:')
+  } 
+  else{
+    if (assgin < nowPageNum) {   //结束页码还需要再判断一下
+      process.stdout.write('结束页码不能小于开始页码\n')
+      process.stdout.write('请输入正确的结束爬取页码:')
+      return
+    }
+    endPageNum = assgin
+    process.stdin.emit('end')
+  }
+})
+
+process.stdin.on('end', () => {
+  process.stdout.write(`开始爬取的页码是${nowPageNum},结束爬取的页码是${endPageNum}\n`)
+  main(baseUrl + nowPageNum)
+})
+
